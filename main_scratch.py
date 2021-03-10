e60299617dd69c4b94477a020819432414e12379
Eldhose Poulose <eldhose980@gmail.com>
2021-03-10 15:27:27 +0100

    function added: Posterior

--- main_scratch.py
+++ main_scratch.py
@@ -13,13 +13,13 @@ here m1, m2, m3 is our feature and class represents Y or N
 
 import numpy as np
 X_train = np.array([
-    [0,1,1],
-    [0,0,1],
-    [0,0,0],
-    [1,1,0]
+    [0, 1, 1],
+    [0, 0, 1],
+    [0, 0, 0],
+    [1, 1, 0]
 ])
-Y_train = ['Y','N','Y','Y']
-X_test = np.array([[1,1,0]])
+Y_train = ['Y', 'N', 'Y', 'Y']
+X_test = np.array([[0, 0, 1]])
 
 def get_label_indices(labels):
     """
@@ -41,7 +41,7 @@ def get_prior(label_indices):
     """
     Compute Prior based on the train data
     :param label_indices: group of sample indices by class
-    :return: dictionary, with class label as key, corresponding prior as the value
+    :return: dictionary, KEY: class label, VALUE: corresponding prior
     """
     prior = {label: len(indices) for label, indices in label_indices.items()}
     total_count = sum(prior.values())
@@ -60,7 +60,7 @@ def get_likelihood(features, label_indices, smoothing=0):
     :param features: matrix of features
     :param label_indices: grouped sample indices by class
     :param smoothing: integer, additive smoothing parameter
-    :return: dictionary, with class as KEY, corresponding conditional probability vector as VALUE
+    :return: dictionary, KEY: class, VALUE: corresponding conditional probability vector
     """
 
     likelihood= {}
@@ -70,4 +70,50 @@ def get_likelihood(features, label_indices, smoothing=0):
         likelihood[label] = likelihood[label] / (total_count + 2 * smoothing)
 
     return likelihood
+
+#smoothing = 1
+likelihood = get_likelihood(X_train, label_indices, smoothing=1)
+#print("Likelihood: \n", likelihood)
+
+def get_posterior(X,prior, likelihood):
+    """
+    This function computes the posterior of testing samples, based on prior and likelihood
+    :param X: Test data
+    :param prior: dictionary, KEY: class label VALUE: corresponding prior
+    :param likelihood: dictionary, KEY: class label VALUE: corresponding conditional probability vector
+    :return: dictionary, KEY: class label VALUE: corresponding Posterior
+    """
+
+    posteriors= []
+    for x in X:
+        #Posterior is proportional to prior * likelihood
+        posterior = prior.copy()
+        for label, likelihood_label in likelihood.items():
+            for index, bool_value in enumerate(x):
+                posterior[label] *= likelihood_label[index] if bool_value else (1-likelihood_label[index])
+
+                #normalize to sum it up to 1: probability sum = 1
+                sum_posterior = sum(posterior.values())
+                for label in posterior:
+                    if posterior[label] == float('inf'):
+                        posterior[label] = 1.0
+                    else:
+                        posterior[label] /= sum_posterior
+                posteriors.append(posterior.copy())
+
+    return posteriors
+
+posterior = get_posterior(X_test, prior, likelihood)
+print('Posterior: \n', posterior)
+
+
+
+
+
+
+
+
+
+
+
 
